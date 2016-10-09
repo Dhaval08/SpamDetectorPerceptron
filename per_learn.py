7e3b1521b1b18e77d2bcc02cb3819b1a5e88cf5d
dhaval08 <shahdhaval1893@gmail.com>
2016-10-09 13:22:30 -0700

    Created the model.txt file which stores the final bias value and weights of each feature after iterating through the training data for given number of times (20 in this case)

--- per_learn.py
+++ per_learn.py
@@ -1,70 +1,79 @@
 import glob, os, sys
 
+modelFile = open("per_model.txt", "w", encoding="latin1")
+
 filename = '/Users/shahdhaval/PycharmProjects/Perceptron/test'
 
 features = {}
 bias = 0
 
+# This function will update the weights of the features present in the training file
+
+def updateFeatureWeights(file, bias, features, check):
+
+   words_in_file = []
+   sum = 0
+   with open(file, "r", encoding="latin1") as f1:
+       for line in f1:
+           for word in line.strip().split():
+               words_in_file.append(word)
+               if word not in features:
+                   features[word] = 0
+               sum = sum + features[word]
+
+   alpha = sum + bias
+
+   if 1 * alpha <= 0:
+       if check == 1:                                               # spam file
+           for word in words_in_file:
+               features[word] = features[word] + 1
+           bias = bias + 1
+
+       if check == -1:                                              # ham file
+           for word in words_in_file:
+               features[word] = features[word] - 1
+           bias = bias - 1
+
+   return bias, features
 
 
-for root, subdirs, files in os.walk(filename):
-    if(os.path.basename(os.path.normpath(root)) == "spam"):
+spam_files = 0
+ham_files = 0
+first_iteration = True
 
+# running through all the training data for the given number of iterations
 
-        print('hello')
-        os.chdir(root)
-        for file in glob.glob("*.txt"):
-            words_in_file = []
-            sum = 0
+for iteration in range (0, 20):
 
-            with open(file, "r", encoding="latin1") as f1:
-                for line in f1:
-                    for word in line.strip().split():
-                        words_in_file.append(word)
+    for root, subdirs, files in os.walk(filename):
+        if(os.path.basename(os.path.normpath(root)) == "spam"):
 
-                        if word not in features:
-                            features[word] = 0
+            os.chdir(root)
+            for file in glob.glob("*.txt"):
+                if first_iteration == True:
+                    spam_files = spam_files + 1
+                bias, features = updateFeatureWeights(file, bias, features, 1)  #this function will return the updated bias and weights
 
-                        sum = sum + features[word]              #get the sum of weights of all the words present in the file
+        elif(os.path.basename(os.path.normpath(root)) == "ham"):
 
-                alpha = sum + bias
+            os.chdir(root)
+            for file in glob.glob("*.txt"):
+                if first_iteration == True:
+                    ham_files = ham_files + 1
+                bias, features = updateFeatureWeights(file, bias, features, -1)  #this function will return the updated bias and weights
 
-        if 1 * alpha <= 0:
+    first_iteration = False
 
-            for word in words_in_file:
-                features[word] = features[word] + 1
+total_files = spam_files + ham_files
 
-            bias = bias + 1
+# printing the learned model to a text file
 
-    elif(os.path.basename(os.path.normpath(root)) == "ham"):
+modelFile.write(str(bias))
+modelFile.write('\n')
 
-        print('hello')
+for feature in features:
+    print_line = feature+" "+str(features[feature])
+    modelFile.write(print_line)
+    modelFile.write('\n')
 
-
-        os.chdir(root)
-
-        for file in glob.glob("*.txt"):
-
-            words_in_file = []
-            sum = 0
-
-            with open(file, "r", encoding="latin1") as f1:
-                for line in f1:
-                    for word in line.strip().split():
-
-                        words_in_file.append(word)
-
-                        if word not in features:
-                            features[word] = 0
-
-                        sum = sum + features[word]              #get the sum of weights of all the words present in the file
-
-                alpha = sum + bias
-
-        if 1 * alpha <= 0:
-            for word in words_in_file:
-                features[word] = features[word] - 1
-
-            bias = bias - 1
-
-print(features)
+modelFile.close()
