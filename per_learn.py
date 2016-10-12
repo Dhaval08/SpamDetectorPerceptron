9f5c4873864aad43408ac477c006cfdea8f859b8
dhaval08 <shahdhaval1893@gmail.com>
2016-10-12 15:32:21 -0700

    Reduced the time complexity ( Still ned to verify the correctness)

--- per_learn.py
+++ per_learn.py
@@ -1,30 +1,30 @@
-import glob, os, sys
+import glob, os, sys, random
 
 modelFile = open("per_model.txt", "w", encoding="latin1")
 
 filename = '/Users/shahdhaval/Desktop/CSCI544/Spam or Ham/train'
 
 bias = 0
-
 # This function will update the weights of the features present in the training file
 
-def updateFeatureWeights(file, bias, features, check):
+def updateFeatureWeights(word_dict, bias, features, check):
 
     sum = 0
-    for word in file:
+    for word in word_dict[0]:
         sum = sum + int(features[word])
 
     alpha = sum + bias
 
-    if check == 1:                                               # spam file
+    if word_dict[1] == 1:
+                                               # spam file
         if 1 * alpha <= 0:
-            for word in file:
+            for word in word_dict[0]:
                 features[word] = features[word] + 1
             bias = bias + 1
 
-    if check == -1:
+    if word_dict[1] == 2:
         if -(1 * alpha) <= 0:                                              # ham file
-            for word in file:
+            for word in word_dict[0]:
                 features[word] = features[word] - 1
             bias = bias - 1
 
@@ -35,11 +35,10 @@ def updateFeatureWeights(file, bias, features, check):
 spam_dict = {}
 ham_dict = {}
 features = {}
-spam_file_count = 1
-ham_file_count = 1
+file_count = 1
 
 for root, subdirs, files in os.walk(filename):
-    if(os.path.basename(os.path.normpath(root)) == "spam"):
+    if(os.path.basename(os.path.normpath(root)) == "spam" or os.path.basename(os.path.normpath(root)) == "ham"):
         os.chdir(root)
         for file in glob.glob("*.txt"):
             words = []
@@ -50,32 +49,22 @@ for root, subdirs, files in os.walk(filename):
                         if word not in features:
                             features[word] = int(0)
 
-            spam_dict[spam_file_count] = words
-            spam_file_count = spam_file_count + 1
+            if(os.path.basename(os.path.normpath(root)) == "spam"):
+                spam_dict[file_count] = words, 1
+                spam_file_count = file_count + 1
 
-    if(os.path.basename(os.path.normpath(root)) == "ham"):
-        os.chdir(root)
-        for file in glob.glob("*.txt"):
-            words = []
-            with open(file, "r", encoding="latin1") as f1:
-                for line in f1:
-                    for word in line.strip().split():
-                        words.append(word)
-                        if word not in features:
-                            features[word] = int(0)
+            if(os.path.basename(os.path.normpath(root)) == "ham"):
+                spam_dict[file_count] = words, 2
+                spam_file_count = file_count + 1
 
-            ham_dict[ham_file_count] = words
-            ham_file_count = ham_file_count + 1
-Reduced the time complexity of the code
+r = list(range(1, len(spam_dict)+1))
+random.shuffle(r)
+
 for iteration in range (0, 20):
-    for i in range (1, len(spam_dict)+1):
+    for i in r:
         bias, features = updateFeatureWeights(spam_dict[i], bias, features, 1)
-    for i in range (1, len(ham_dict)+1):
-        bias, features = updateFeatureWeights(ham_dict[i], bias, features, -1)
 
 # printing the learned model to a text file
-
-print(bias)
 
 modelFile.write(str(bias))
 modelFile.write('\n')
