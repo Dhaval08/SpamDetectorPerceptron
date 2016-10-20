3a0a4240f7901908f12f66c0e6c52d43c200cde5
dhaval08 <shahdhaval1893@gmail.com>
2016-10-20 14:28:55 -0700

    Added per_classify

--- per_learn.py
+++ per_learn.py
@@ -2,7 +2,7 @@ import glob, os, sys, random
 
 modelFile = open("per_model.txt", "w", encoding="latin1")
 
-filename = '/Users/shahdhaval/Desktop/CSCI544/Spam or Ham/train'
+filename = ' '.join(sys.argv[1:])
 
 bias = 0
 # This function will update the weights of the features present in the training file
@@ -15,21 +15,19 @@ def updateFeatureWeights(word_dict, bias, features, check):
 
     alpha = sum + bias
 
-    if word_dict[1] == 1:                                             # spam file
+    if word_dict[2] == 1:                                             # spam file
         if 1 * alpha <= 0:
-            for word in word_dict[0]:
+            for word in word_dict[1]:
                 features[word] = features[word] + 1
             bias = bias + 1
 
-    if word_dict[1] == 2:
+    if word_dict[2] == 2:
         if -(1 * alpha) <= 0:                                              # ham file
-            for word in word_dict[0]:
+            for word in word_dict[1]:
                 features[word] = features[word] - 1
             bias = bias - 1
 
     return bias, features
-
-# running through all the training data for the given number of iterations
 
 spam_dict = {}
 ham_dict = {}
@@ -41,26 +39,31 @@ for root, subdirs, files in os.walk(filename):
         os.chdir(root)
         for file in glob.glob("*.txt"):
             words = []
+            unique_words=[]
             with open(file, "r", encoding="latin1") as f1:
                 contents = f1.readlines()
                 for line in range(len(contents)):
                     for word in contents[line].strip().split():
+                        if word not in unique_words:
+                            unique_words.append(word)
                         words.append(word)
                         if word not in features:
                             features[word] = int(0)
 
             if(os.path.basename(os.path.normpath(root)) == "spam"):
-                spam_dict[file_count] = words, 1
+                spam_dict[file_count] = words, unique_words, 1
                 file_count = file_count + 1
 
             elif(os.path.basename(os.path.normpath(root)) == "ham"):
-                spam_dict[file_count] = words, 2
+                spam_dict[file_count] = words, unique_words, 2
                 file_count = file_count + 1
 
-r = list(range(1, len(spam_dict)+1))
-random.shuffle(r)
+# running through all the training data for the given number of iterations
 
 for iteration in range (0, 20):
+
+    r = list(range(1, len(spam_dict)+1))
+    random.shuffle(r)
     for i in r:
         bias, features = updateFeatureWeights(spam_dict[i], bias, features, 1)
 
